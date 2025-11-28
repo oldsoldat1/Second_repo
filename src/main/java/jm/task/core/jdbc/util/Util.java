97f6e21b2d37efd618af05d22c1d33c2e90e2466
oldsoldat1 <oldsoldat1@gmail.com>
2025-11-28 18:02:58 +0600

    Переисправил Util класс

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,21 +6,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/my_bd_test";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            Class.forName(DRIVER);
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Соединение с БД установлено");
 
-        } catch (SQLException | ClassNotFoundException e) {
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println("Соединение с БД установлено");
+            return conn;
+
+        } catch (SQLException e) {
             throw new RuntimeException("Нет соединения с базой данных", e);
         }
-        return conn;
+
+
     }
 }
